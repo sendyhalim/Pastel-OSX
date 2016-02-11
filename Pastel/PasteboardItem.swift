9cab476237c127742a703a4cc2409f714db8c8ff
Sendy Halim <sendyhalim93@gmail.com>
2016-02-11 22:57:31 +0700

    Add PasteboardItem equality operator

--- Pastel/PasteboardItem.swift
+++ Pastel/PasteboardItem.swift
@@ -13,3 +13,23 @@ enum PasteboardItem {
   case URL(NSURL)
   case Image(NSImage)
 }
+
+func == (lhs: PasteboardItem, rhs: PasteboardItem) -> Bool {
+  switch (lhs, rhs) {
+  case (.Text(let str1), .Text(let str2)):
+    return str1 == str2
+
+  case (.URL(let url1), .URL(let url2)):
+    return url1 == url2
+
+  case (.Image(let image1), .Image(let image2)):
+    return image1.isEqual(image2)
+
+  default:
+    return false
+  }
+}
+
+func != (lhs: PasteboardItem, rhs: PasteboardItem) -> Bool {
+  return !(lhs == rhs)
+}
