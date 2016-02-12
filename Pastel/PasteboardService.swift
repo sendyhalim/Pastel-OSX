d15c7e68b38ccf75a8e2934d63b0d6ef9f28a98e
Sendy Halim <sendyhalim93@gmail.com>
2016-02-13 01:58:08 +0700

    Add PasteboardService

---
+++ Pastel/PasteboardService.swift
@@ -0,0 +1,60 @@
+//
+//  PasteboardService.swift
+//  Pastel
+//
+//  Created by Sendy Halim on 2/11/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+import RxSwift
+import Swiftz
+
+class PasteboardService {
+  let pasteboard = NSPasteboard.generalPasteboard()
+  let pasteboardItems = Variable([PasteboardItem]())
+  let changeCount = Variable(0)
+  let disposeBag = DisposeBag()
+
+  func pollPasteboardItems() {
+    if changeCount.value == pasteboard.changeCount {
+      return
+    }
+
+    let _items = pasteboard.readObjectsForClasses(
+      [NSString.self, NSImage.self, NSURL.self],
+      options: nil
+    )
+
+    guard let items = _items where items.count > 0 else {
+        return
+    }
+
+    items.first >>- pasteboardItem >>- addPasteboardItem
+  }
+
+  func pasteboardItem(item: AnyObject) -> PasteboardItem? {
+    if let string = item as? String {
+      return .Text(string)
+    }
+
+    if let image = item as? NSImage {
+      return .Image(image)
+    }
+
+    if let url = item as? NSURL {
+      return .URL(url)
+    }
+
+    return Optional.None
+  }
+
+  private func addPasteboardItem(item: PasteboardItem) {
+    pasteboardItems.value = pasteboardItems.value.filter {
+      $0 != item
+    }
+
+    pasteboardItems.value.append(item)
+    changeCount.value = pasteboard.changeCount
+  }
+}
