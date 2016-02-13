a768fc86f4175511734a0b6d2772c47719100f33
Sendy Halim <sendyhalim93@gmail.com>
2016-02-13 16:10:49 +0700

    Add some testing code in view controller

--- Pastel/ViewController.swift
+++ Pastel/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import Cocoa
+import RxSwift
+import RxCocoa
 
 class ViewController: NSViewController {
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    let pasteboardService = PasteboardService()
+
+    pasteboardService.pasteboardItems.asDriver().driveNext {
+      print("Paste board items: \($0)")
+    }
+    .addDisposableTo(disposeBag)
+
+    Observable.interval(1, scheduler: MainScheduler.instance)
+      .subscribeNext {
+        (second: Int) in
+        print("Polling at second \(second)")
+        pasteboardService.pollPasteboardItems()
+      }
+      .addDisposableTo(disposeBag)
 
     // Do any additional setup after loading the view.
   }
