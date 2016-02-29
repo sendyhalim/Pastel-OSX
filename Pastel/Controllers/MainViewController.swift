29124d07babb1bf18d02f1654f27c2cd8cf100c8
Sendy Halim <sendyhalim93@gmail.com>
2016-02-29 23:42:31 +0700

    Add MainViewController and its xib

---
+++ Pastel/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Pastel
+//
+//  Created by Sendy Halim on 2/29/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+  @IBOutlet weak var contentView: NSView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do view setup here.
+  }
+}
