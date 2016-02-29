80baeae68176ee313bdaa9a732c69952533620a5
Sendy Halim <sendyhalim93@gmail.com>
2016-03-01 01:08:31 +0700

    Add quit menu item

--- Pastel/Controllers/MainViewController.swift
+++ Pastel/Controllers/MainViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import Cocoa
+import RxSwift
+import RxCocoa
 
 class MainViewController: NSViewController {
   @IBOutlet weak var contentView: NSView!
   @IBOutlet weak var settingsButton: NSButton!
+  @IBOutlet var settingsMenu: NSMenu!
 
   let pasteboardCollectionViewController = {
     return PasteboardCollectionViewController(
@@ -19,8 +22,25 @@ class MainViewController: NSViewController {
     )!
   }()
 
+  let disposeBag = DisposeBag()
+
   override func awakeFromNib() {
     settingsButton.image?.template = true
     contentView.addSubview(pasteboardCollectionViewController.view)
+
+    settingsButton
+      .rx_tap
+      .subscribeNext { [unowned self] in
+        self.settingsMenu.popUpMenuPositioningItem(
+          self.settingsMenu.itemAtIndex(0),
+          atLocation: NSEvent.mouseLocation() + CGPoint(x: 10, y: -10),
+          inView: nil
+        )
+      }
+      .addDisposableTo(disposeBag)
   }
+}
+
+func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+  return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
 }
